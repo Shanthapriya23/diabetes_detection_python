511283bbaa286d50461dc116d507b0952485dbeb
Shanthapriya23 <118118873+Shanthapriya23@users.noreply.github.com>
2024-01-16 01:04:39 +0530

    Update diabetes_prediction_webapp.py

--- diabetes_prediction_webapp.py
+++ diabetes_prediction_webapp.py
@@ -9,7 +9,7 @@ import pickle
 import streamlit as st
 
 #loading the saved model
-loaded_model = pickle.load(open('E:/Machine_learning/trained_model.sav','rb'))
+loaded_model = pickle.load(open('trained_model.sav','rb'))
 
 def diabetes_prediction(input_data):
     #convert data to numpy array:
